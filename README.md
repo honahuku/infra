33b3173f5a372520158ddb5deafa4b54c713a1eb
Honahuku <tech@honahuku.com>
2023-12-10 05:51:14 +0900

    feat: terraofrm init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # infra
+## init
+```bash
+terraform init \
+-backend-config="bucket=${TF_VAR_bucket_name}"
+```
---
+++ service.tf
@@ -0,0 +1,21 @@
+variable "services" {
+  description = "Cloud APIで有効化するサービスのリスト"
+  type        = list(string)
+  default = [
+    "apigateway.googleapis.com",
+    "iam.googleapis.com",
+    "servicecontrol.googleapis.com",
+    "compute.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "project_service" {
+  for_each = toset(var.services)
+
+  service            = each.value
+  disable_on_destroy = false
+
+  timeouts {
+    create = "5m"
+  }
+}
