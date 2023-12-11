8bfc4c502f466f557679329522ef6e9eb7ff3582
Honahuku <tech@honahuku.com>
2023-12-11 11:56:46 +0900

    feat: helmの設定を入れる

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/google"
       version = "5.7.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.11.0"
+    }
   }
 
   # backend の値は Partial Configuration で init 時に渡す
@@ -14,4 +18,14 @@ terraform {
 
 provider "google" {
   project = var.PROJECT_NAME
+}
+
+provider "helm" {
+  kubernetes {
+    host = var.KUBE_HOST
+
+    client_certificate     = base64decode(var.KUBE_CLIENT_CERT)
+    client_key             = base64decode(var.KUBE_CLIENT_KEY)
+    cluster_ca_certificate = base64decode(var.KUBE_CLUSTER_CA_CERT)
+  }
 }
