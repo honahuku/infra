dec56fb67ce85c62c2c1ba62fa9975a5e14ff3b7
Honahuku <tech@honahuku.com>
2023-12-11 11:56:53 +0900

    feat: argocdを入れる

---
+++ helm.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "argocd" {
+  name = "argocd"
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  skip_crds        = true
+  version          = "5.51.6"
+  create_namespace = true
+
+  #   set {
+  #     name  = "service.type"
+  #     value = "ClusterIP"
+  #   }
+}
